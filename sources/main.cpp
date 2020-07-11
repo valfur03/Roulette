70c1df4fa60d8844c180ba91802dab970c233ce2
Valentin <valentin.furmanek@gmail.com>
2020-07-11 19:17:41 +0200

    Add the start menu

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
+int menu();
+
 int main() {
-	std::cout << "Hellow world!" << std::endl;
+	std::cout << menu() << std::endl;
 
 	return 0;
+}
+
+int menu() {
+	std::cout << "===== MENU =====" << std::endl;
+	std::cout << "1. Jouer" << std::endl;
+	std::cout << "2. Parametres" << std::endl;
+	std::cout << "3. Règles" << std::endl;
+	std::cout << "4. Quitter" << std::endl;
+
+	std::cout << "Entrez un nombre -> ";
+
+	int choice = 0;
+
+	do {
+		std::cin >> choice;
+
+		if (choice < 1 || choice > 4) {
+			std::cout << "Ce nombre n'est pas valide..." << std::endl;
+		}
+	} while (choice < 1 || choice > 4);
+
+	return choice;
 }
