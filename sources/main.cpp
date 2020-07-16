4a23193e53f92e8f2efe4651d480f2b13489e614
Valentin <valentin.furmanek@gmail.com>
2020-07-16 09:49:30 +0200

    Add the choice handler

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,7 +3,16 @@
 int menu();
 
 int main() {
-	std::cout << menu() << std::endl;
+	switch (menu()) {
+	case 1:
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;
+	}
 
 	return 0;
 }
@@ -13,7 +22,6 @@ int menu() {
 	std::cout << "1. Jouer" << std::endl;
 	std::cout << "2. Parametres" << std::endl;
 	std::cout << "3. Règles" << std::endl;
-	std::cout << "4. Quitter" << std::endl;
 
 	std::cout << "Entrez un nombre -> ";
 
@@ -22,10 +30,10 @@ int menu() {
 	do {
 		std::cin >> choice;
 
-		if (choice < 1 || choice > 4) {
+		if (choice < 1 || choice > 3) {
 			std::cout << "Ce nombre n'est pas valide..." << std::endl;
 		}
-	} while (choice < 1 || choice > 4);
+	} while (choice < 1 || choice > 3);
 
 	return choice;
 }
